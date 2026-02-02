361bafcb7d462bd65cf790c25b7f0e2b314f09dd
Binh05 <binhdt0452@gmail.com>
2026-02-02 18:55:36 +0700

    feature/add redux persist

--- src/app/StoreProvider.tsx
+++ src/app/StoreProvider.tsx
@@ -3,15 +3,25 @@
 import { AppStore, makeStore } from "@/lib/store";
 import { useRef } from "react";
 import { Provider } from "react-redux";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
 
 function StoreProvider({ children }: { children: React.ReactNode }) {
-    const storeRef = useRef<AppStore>(undefined);
+  const storeRef = useRef<AppStore>(undefined);
+  const persistRef = useRef<any>(null);
 
-    if (!storeRef.current) {
-        storeRef.current = makeStore();
-    }
+  if (!storeRef.current) {
+    storeRef.current = makeStore();
+    persistRef.current = persistStore(storeRef.current);
+  }
 
-    return <Provider store={storeRef.current}>{children}</Provider>;
+  return (
+    <Provider store={storeRef.current}>
+      <PersistGate loading={null} persistor={persistRef.current}>
+        {children}
+      </PersistGate>
+    </Provider>
+  );
 }
 
 export default StoreProvider;
--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,14 +1,30 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { authReducer } from "./features/authSlice";
 import { chatReducer } from "./features/chatSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["authReducer"],
+};
+
+const rootReducer = combineReducers({
+  authReducer,
+  chatReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const makeStore = () => {
-    return configureStore({
-        reducer: {
-            authReducer,
-            chatReducer,
-        },
-    });
+  return configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        serializableCheck: false,
+      }),
+  });
 };
 
 export type AppStore = ReturnType<typeof makeStore>;
