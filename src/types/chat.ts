13cd71335686d5c151bce760a52ac7dffc5fc52a
Binh05 <binhdt0452@gmail.com>
2026-02-03 16:41:57 +0700

    change chatSlice.tsx -> chatSlice.ts

--- src/types/chat.ts
+++ src/types/chat.ts
@@ -1,56 +1,56 @@
 export interface Participant {
-    _id: string;
-    username: string;
-    avatarUrl?: string | null;
-    joinedAt: string;
+  _id: string;
+  username: string;
+  avatarUrl?: string;
+  joinedAt: string;
 }
 
 export interface SeenUser {
-    _id: string;
-    username?: string;
-    avatarUrl?: string | null;
+  _id: string;
+  username?: string;
+  avatarUrl?: string | null;
 }
 
 export interface Group {
-    name: string;
-    createdBy: string;
+  name: string;
+  createdBy: string;
 }
 
 export interface LastMessage {
+  _id: string;
+  content: string;
+  createdAt: string;
+  sender: {
     _id: string;
-    content: string;
-    createdAt: string;
-    sender: {
-        _id: string;
-        username: string;
-        avatarUrl?: string | null;
-    };
+    username: string;
+    avatarUrl?: string | null;
+  };
 }
 
 export interface Conversation {
-    _id: string;
-    type: "direct" | "group";
-    group: Group;
-    participants: Participant[];
-    lastMessageAt: string;
-    seenBy: SeenUser[];
-    lastMessage: LastMessage | null;
-    unreadCounts: Record<string, number>; // key = userId, value = unread count
-    createdAt: string;
-    updatedAt: string;
+  _id: string;
+  type: "direct" | "group";
+  group: Group;
+  participants: Participant[];
+  lastMessageAt: string;
+  seenBy: SeenUser[];
+  lastMessage: LastMessage | null;
+  unreadCounts: Record<string, number>; // key = userId, value = unread count
+  createdAt: string;
+  updatedAt: string;
 }
 
 export interface ConversationResponse {
-    conversations: Conversation[];
+  conversations: Conversation[];
 }
 
 export interface Message {
-    _id: string;
-    conversationId: string;
-    senderId: string;
-    content: string | null;
-    imgUrl?: string | null;
-    updatedAt?: string | null;
-    createdAt: string;
-    isOwn?: boolean;
+  _id: string;
+  conversationId: string;
+  senderId: string;
+  content: string | null;
+  imgUrl?: string | null;
+  updatedAt?: string | null;
+  createdAt: string;
+  isOwn?: boolean;
 }
