4009b3e2a5f08a683d980e7402625750dd882460
Binh05 <binhdt0452@gmail.com>
2026-03-26 23:48:41 +0700

    feat: add rewrites in next config to call api on server

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api-proxy/:path*",
+        destination: "http://localhost:5000/v1/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
--- src/services/authService.ts
+++ src/services/authService.ts
@@ -2,13 +2,37 @@ import { api } from "@/lib/api";
 
 export const authService = {
   signOut: async () => {
-    await api.post("/auth/signout");
+    // await api.post("/auth/signout");
+    const res = await fetch("/api-proxy/auth/signout", {
+      method: "POST",
+      credentials: "include",
+    });
+
+    if (res.status != 204) {
+      throw new Error("Lỗi khi đăng xuất");
+    }
   },
 
   signIn: async (email: string, password: string) => {
-    const res = await api.post(`/auth/signin`, { email, password });
+    //const res = await api.post(`/auth/signin`, { email, password });
+    const res = await fetch("/api-proxy/auth/signin", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+      credentials: "include",
+    });
 
-    return res.data;
+    const data = await res.json();
+
+    if (!res.ok) {
+      throw new Error("Loi khi dang nhap");
+    }
+
+    console.log("signin data", data);
+
+    return data;
   },
 
   signUp: async (username: string, email: string, password: string) => {
